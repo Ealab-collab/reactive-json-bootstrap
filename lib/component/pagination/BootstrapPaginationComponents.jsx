afda82187e8c07916d8b8f5de486d792a448ce22
Quang-Minh DANG <quang-minh@ea-lab.io>
2025-08-22 19:01:24 +0200

    main: pagination.

---
+++ lib/component/pagination/BootstrapPaginationComponents.jsx
@@ -0,0 +1,24 @@
+import { Pagination } from "react-bootstrap";
+
+export const BootstrapPaginationContainer = ({children, ...props}) => (
+    <Pagination {...props}>
+        {children}
+    </Pagination>
+);
+
+export const BootstrapPaginationEllipsis = Pagination.Ellipsis;
+export const BootstrapPaginationItem = Pagination.Item;
+export const BootstrapPaginationFirst = Pagination.First;
+export const BootstrapPaginationLast = Pagination.Last;
+export const BootstrapPaginationNext = Pagination.Next;
+export const BootstrapPaginationPrev = Pagination.Prev;
+
+export const bootstrapPaginationPlugin = {
+    Container: BootstrapPaginationContainer,
+    Ellipsis: BootstrapPaginationEllipsis,
+    First: BootstrapPaginationFirst,
+    Item: BootstrapPaginationItem,
+    Last: BootstrapPaginationLast,
+    Next: BootstrapPaginationNext,
+    Prev: BootstrapPaginationPrev,
+};
--- lib/main.jsx
+++ lib/main.jsx
@@ -2,11 +2,12 @@
  * Entry point for the lib.
  */
 import * as elementComponents from "./component/element";
+import { bootstrapPaginationPlugin } from "./component/pagination/BootstrapPaginationComponents";
 
 export const bootstrapComponentsPlugin = {
-    element: {
-        ...elementComponents,
-    },
+    element: elementComponents,
+    pagination: bootstrapPaginationPlugin,
 };
 
 export * from "./component/element";
+export * from "./component/pagination";
