8720285eda5a16e571b118f188882d7db67b3708
Quang-Minh DANG <quang-minh@ea-lab.io>
2025-08-25 11:25:20 +0200

    main: Popover and Tooltip wrappers.

--- lib/main.jsx
+++ lib/main.jsx
@@ -1,13 +1,14 @@
 /**
  * Entry point for the lib.
  */
+import * as actionComponents from "./component/action";
 import * as elementComponents from "./component/element";
 import { bootstrapPaginationPlugin } from "./component/pagination/BootstrapPaginationComponents";
 
 export const bootstrapComponentsPlugin = {
+    action: actionComponents,
     element: elementComponents,
     pagination: bootstrapPaginationPlugin,
 };
 
-export * from "./component/element";
-export * from "./component/pagination";
+export * from "./component";
